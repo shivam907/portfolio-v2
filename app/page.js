332a8b7fed090be733fed0fb9566b857532d12f7
shivam907 <shivamkaushal907@gmail.com>
2023-07-11 10:27:30 +0530

    Added Footer

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import styles from "./page.module.css";
 import NavBar from "@/components/NavBar";
 import Hero from "../components/Hero";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <>
       <NavBar />
       <Hero />
+      <Footer />
     </>
   );
 }
---
+++ components/Footer.js
@@ -0,0 +1,23 @@
+import React from "react";
+import classes from "./Footer.module.css";
+const Footer = () => {
+  return (
+    <footer className={classes.footer}>
+      <div className={classes.footerLeft}>
+        <div className={classes.footerLink}>find me in:</div>
+        <div className={classes.footerLink}>
+          <img src="/icons8-instagram (1).svg" alt="" />
+        </div>
+        <div className={classes.footerLink}>
+          <img src="/icons8-linkedin.svg" alt="" />
+        </div>
+      </div>
+      <div className={classes.github}>
+        <div className={classes.footerLink1}>shivam907</div>
+        <img src="/icons8-github.svg" alt="" />
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
