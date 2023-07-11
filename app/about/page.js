3bc22723c387c242775b9274b0a51f5c8cb69b28
shivam907 <shivamkaushal907@gmail.com>
2023-07-11 13:08:35 +0530

    In the About Section Left side bar done

---
+++ app/about/page.js
@@ -0,0 +1,121 @@
+import React from "react";
+import NavBar from "@/components/NavBar";
+import Footer from "@/components/Footer";
+import classes from "./about.module.css";
+const page = () => {
+  return (
+    <>
+      <div className={classes.leftBar}>
+        <img className={classes.img1} src="/personal-info-icon.svg" alt="" />
+        <img
+          className={classes.img2}
+          src="/professional-info-icon.svg"
+          alt=""
+        />
+      </div>
+      <div className={classes.info}>
+        <div className={classes.personalInfo}>
+          <div className={classes.heading}>
+            <div className={classes.icon}>
+              <img src="/icon.png" alt="" />
+            </div>
+            <div className={classes.name}>personal-info</div>
+          </div>
+          <div className={classes.folders}>
+            <div className={classes.folder}>
+              <div className={classes.folderIcon}>
+                <img src="/folderIcon.png" alt="" />
+              </div>
+              <div className={classes.folderImage}>
+                <img src="/folder1.png" alt="" />
+              </div>
+              <div className={classes.folderName}>bio</div>
+              <div className={classes.folderContent}>
+                <div className={classes.file}>
+                  <div className={classes.fileIcon}>
+                    <img src="/markdown.png" alt="" />
+                  </div>
+                  <div className={classes.fileName}>high-school</div>
+                </div>
+                <div className={classes.file}>
+                  <div className={classes.fileIcon}>
+                    <img src="/markdown.png" alt="" />
+                  </div>
+                  <div className={classes.fileName}>high-school</div>
+                </div>
+              </div>
+            </div>
+            <div className={classes.folder}>
+              <div className={classes.folderIcon}>
+                <img src="/folderIcon.png" alt="" />
+              </div>
+              <div className={classes.folderImage}>
+                <img src="/folder2.png" alt="" />
+              </div>
+              <div className={classes.folderName}>interests</div>
+              <div className={classes.folderContent}>
+                <div className={classes.file}>
+                  <div className={classes.fileIcon}>
+                    <img src="/markdown.png" alt="" />
+                  </div>
+                  <div className={classes.fileName}>high-school</div>
+                </div>
+                <div className={classes.file}>
+                  <div className={classes.fileIcon}>
+                    <img src="/markdown.png" alt="" />
+                  </div>
+                  <div className={classes.fileName}>high-school</div>
+                </div>
+              </div>
+            </div>
+            <div className={classes.folder}>
+              <div className={classes.folderIcon}>
+                <img src="/folderIcon.png" alt="" />
+              </div>
+              <div className={classes.folderImage}>
+                <img src="/folder3.png" alt="" />
+              </div>
+              <div className={classes.folderName}>education</div>
+              <div className={classes.folderContent}>
+                <div className={classes.file}>
+                  <div className={classes.fileIcon}>
+                    <img src="/markdown.png" alt="" />
+                  </div>
+                  <div className={classes.fileName}>high-school</div>
+                </div>
+                <div className={classes.file}>
+                  <div className={classes.fileIcon}>
+                    <img src="/markdown.png" alt="" />
+                  </div>
+                  <div className={classes.fileName}>high-school</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={classes.contacts}>
+          <div className={classes.heading}>
+            <div className={classes.icon}></div>
+            <div className={classes.name}>personal-info</div>
+          </div>
+          <div className={classes.folders}>
+            <div className={classes.folder}>
+              <div className={classes.fileIcon}>
+                <img src="/mail-icon.png" alt="" />
+              </div>
+              <div className={classes.fileName}>user@gmail.com</div>
+            </div>
+            <div className={classes.folder}>
+              <div className={classes.fileIcon}>
+                <img src="/phone-icon.png" alt="" />
+              </div>
+              <div className={classes.fileName}>+91 8968969929</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default page;
--- app/page.js
+++ app/page.js
@@ -6,9 +6,9 @@ import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <>
-      <NavBar />
+      {/* <NavBar /> */}
       <Hero />
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 }
