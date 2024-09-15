1676744d5c9b73d5df34eb229ad7bdadd4f40f15
shivam907 <shivamkaushal907@gmail.com>
2024-09-15 21:14:52 +0530

    made the filters working

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,23 +1,36 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import classes from "./project.module.css";
 import Link from "next/link";
-import File1 from "../../components/File1";
-import File2 from "../../components/File2";
-import File3 from "../../components/File3";
-const page = () => {
-  const [file, changeFile] = React.useState(File3);
-  const [folder1, changeFolder1] = React.useState(true);
-  const [folder2, changeFolder2] = React.useState(false);
-  const [folder3, changeFolder3] = React.useState(false);
+import projectss from "../../data/projects.json";
 
-  const [workspace, changeWorkspace] = React.useState(true);
+const Page = () => {
+  const [projects, setProjects] = useState(projectss);
+  const [selectedTechnologies, setSelectedTechnologies] = useState({
+    HTML: true,
+    CSS: true,
+    ReactJs: true,
+    NodeJs: true,
+    JavaScript: true,
+    NextJs: true,
+  });
 
-  const [fileActive, changeFileActive] = React.useState(1);
-  const fileNames = ["bio.md", "interests.md", "education.md"];
-  React.useEffect(() => {
-    changeFile(fileActive == 1 ? File1 : fileActive == 2 ? File2 : File3);
-  }, [fileActive]);
+  // Handle checkbox change
+  const handleCheckboxChange = (tech) => {
+    setSelectedTechnologies((prevState) => ({
+      ...prevState,
+      [tech]: !prevState[tech],
+    }));
+  };
+
+  // Filter projects based on selected technologies
+  const filteredProjects = projects.filter((project) => {
+    const projectTechs = project.technologies || []; // Ensure projects have a technologies array
+    return Object.keys(selectedTechnologies).some(
+      (tech) => selectedTechnologies[tech] && projectTechs.includes(tech)
+    );
+  });
+
   return (
     <>
       <div className={classes.leftBar}>
@@ -30,182 +43,72 @@ const page = () => {
       </div>
       <div className={classes.info}>
         <div className={classes.personalInfo}>
-          <div
-            className={classes.heading}
-            onClick={() => {
-              changeWorkspace(workspace ? false : true);
-            }}
-          >
+          <div className={classes.heading}>
             <div className={classes.icon}>
-              <img
-                className={workspace ? "" : classes.rotated}
-                src="/icon.webp"
-                alt=""
-              />
+              <img src="/icon.webp" alt="" />
             </div>
             <div className={classes.name}>my-projects</div>
           </div>
-          {workspace && (
-            <div className={classes.select}>
-              <div className={classes.option}>
-                <label className={classes.check}>
-                  <input
-                    type="checkbox"
-                    className={classes.checkbox}
-                    // checked="checked"
-                  />
-                </label>
-
-                <div className={classes.selectOption}>
-                  <img src="/html.webp" alt="" />
+          <div className={classes.select}>
+            {["HTML", "CSS", "ReactJs", "NodeJs", "JavaScript", "NextJs"].map(
+              (tech) => (
+                <div key={tech} className={classes.option}>
+                  <label className={classes.check}>
+                    <input
+                      type="checkbox"
+                      className={classes.checkbox}
+                      checked={selectedTechnologies[tech]}
+                      onChange={() => handleCheckboxChange(tech)}
+                    />
+                  </label>
+                  <div className={classes.optionName}>{tech}</div>
                 </div>
-                <div className={classes.optionName}>HTML</div>
-              </div>
-              <div className={classes.option}>
-                <label className={classes.check}>
-                  <input
-                    type="checkbox"
-                    className={classes.checkbox}
-                    // checked="checked"
-                  />
-                </label>
-
-                <div className={classes.selectOption}>
-                  <img src="/css.webp" alt="" />
-                </div>
-                <div className={classes.optionName}>CSS</div>
-              </div>
-              <div className={classes.option}>
-                <label className={classes.check}>
-                  <input
-                    type="checkbox"
-                    className={classes.checkbox}
-                    // checked="checked"
-                  />
-                </label>
-
-                <div className={classes.selectOption}>
-                  <img src="/react.webp" alt="" />
-                </div>
-                <div className={classes.optionName}>React Js</div>
-              </div>
-              <div className={classes.option}>
-                <label className={classes.check}>
-                  <input
-                    type="checkbox"
-                    className={classes.checkbox}
-                    // checked="checked"
-                  />
-                </label>
-
-                <div className={classes.selectOption}>
-                  <img className={classes.node} src="/node.webp" alt="" />
-                </div>
-                <div className={classes.optionName}>Node Js</div>
-              </div>
-              <div className={classes.option}>
-                <label className={classes.check}>
-                  <input
-                    type="checkbox"
-                    className={classes.checkbox}
-                    // checked="checked"
-                  />
-                </label>
-
-                <div className={classes.selectOption}>
-                  <img src="/js.webp" alt="" />
-                </div>
-                <div className={classes.optionName}>JavaScript</div>
-              </div>
-            </div>
-          )}
+              )
+            )}
+          </div>
         </div>
       </div>
       <div className={classes.code}>
-        <div className={classes.upperBar}>
-          <div className={classes.fileOpened}>
-            <div className={classes.fileOpenedName}>Projects.md</div>
-            <div className={classes.fileIcons}>
-              <img src="/close-icon.webp" alt="" />
-            </div>
-          </div>
-        </div>
         <div className={classes.displayFile}>
-          <div className={classes.project}>
-            <div className={classes.projNames}>
-              <div className={classes.projName}>Project 1</div>
-              <div className={classes.projectName}>// _audiohile-ecommerce</div>
-            </div>
-            <div className={classes.projectBox}>
-              <img src="/proj1.webp" alt="" />
-              <div className={classes.projContent}>
-                <p>Duis aute irure dolor in velit esse cillum dolore.</p>{" "}
-                <div className={classes.btns}>
-                  <Link href="https://lovely-gumption-003323.netlify.app/">
-                    <div className={classes.view}>Live Demo</div>
-                  </Link>
-                  <Link
-                    className={classes.imm}
-                    href="https://github.com/shivam907/audiophile"
-                  >
-                    {/* <div className={classes.imm}> */}
-                    <img src="/icons8-github.svg" alt="" />
-                    <div className={classes.cir}></div>
-                    {/* </div> */}
-                  </Link>
+          {filteredProjects.length > 0 ? (
+            filteredProjects.map((project) => (
+              <div key={project.id} className={classes.project}>
+                <div className={classes.projNames}>
+                  <div className={classes.projName}>{project.name}</div>
+                  <div className={classes.projectName}>
+                    // _{project.nameClass}
+                  </div>
                 </div>
-              </div>
-            </div>
-          </div>
-          <div className={classes.project}>
-            <div className={classes.projNames}>
-              <div className={classes.projName}>Project 2</div>
-              <div className={classes.projectName}>// _metaverse</div>
-            </div>
-            <div className={classes.projectBox}>
-              <img src="/proj2.webp" alt="" />
-              <div className={classes.projContent}>
-                <p>Duis aute irure dolor in velit esse cillum dolore.</p>
-                <div className={classes.btns}>
-                  <Link href="https://metaverse-henna.vercel.app/">
-                    <div className={classes.view}>Live Demo</div>
-                  </Link>
-                  {/* <div className={classes.imm}> */}
-                  <Link
-                    className={classes.imm}
-                    href="https://github.com/shivam907/metaverse"
-                  >
-                    <img src="/icons8-github.svg" alt="" />
-                    <div className={classes.cir}></div>
-                    {/* </div> */}
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className={classes.project}>
-            <div className={classes.projNames}>
-              <div className={classes.projName}>Project 3</div>
-              <div className={classes.projectName}>// _snippet-saver</div>
-            </div>
-            <div className={classes.projectBox}>
-              <img src="/proj3.webp" alt="" />
-              <div className={classes.projContent}>
-                <p>Duis aute irure dolor in velit esse cillum dolore.</p>{" "}
-                <div className={classes.btns}>
-                  <div className={classes.view}>Live Demo</div>
-                  <div className={classes.imm}>
-                    <img src="/icons8-github.svg" alt="" />
-                    <div className={classes.cir}></div>
+                <div className={classes.projectBox}>
+                  <img src={project.image} alt={project.name} />
+                  <div className={classes.projContent}>
+                    <p>{project.description}</p>
+                    <div className={classes.btns}>
+                      {project.projectLink && (
+                        <Link href={project.projectLink}>
+                          <div className={classes.view}>Live Demo</div>
+                        </Link>
+                      )}
+                      {project.githubLink && (
+                        <Link className={classes.imm} href={project.githubLink}>
+                          <img src={project.repoIcon} alt="GitHub" />
+                          <div className={classes.cirr}>
+                            <div className={classes.cir}></div>
+                          </div>
+                        </Link>
+                      )}
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </div>
+            ))
+          ) : (
+            <p>No projects match the selected technologies.</p>
+          )}
         </div>
       </div>
     </>
   );
 };
 
-export default page;
+export default Page;
