4e02f920b4db335085a46df55b764e1e9a8ff6e0
shivam907 <shivamkaushal907@gmail.com>
2024-09-25 22:18:31 +0530

    added speed component

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from '@vercel/analytics/react';
 export const metadata = {
   title: "Shivam Kaushal",
@@ -14,9 +14,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-
-    <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.003" />
+        <meta charset="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.003"
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
@@ -30,6 +32,7 @@ export default function RootLayout({ children }) {
         <Analytics />
         <Footer />
       </body>
+      <SpeedInsights />
     </html>
   );
 }
