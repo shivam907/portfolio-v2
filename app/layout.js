c344946459ff67136bd2c37f1d80f40965eab0ee
shivam907 <shivamkaushal907@gmail.com>
2024-09-25 22:46:20 +0530

    added speed component

--- app/layout.js
+++ app/layout.js
@@ -12,27 +12,29 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <head>
-        <meta charset="utf-8" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.003"
-        />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <body>
-        <NavBar />
-        <main>{children}</main>
-        <Analytics />
-        <Footer />
-      </body>
+    <>
+      <html lang="en">
+        <head>
+          <meta charset="utf-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.003"
+          />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
+        </head>
+        <body>
+          <NavBar />
+          <main>{children}</main>
+          <Analytics />
+          <Footer />
+        </body>
+      </html>
       <SpeedInsights />
-    </html>
+    </>
   );
 }
